9268da1763702f2d63b525747a1235e439e8d10a
Liam <liam@mail.com>
2018-01-30 14:13:48 +1100

    Signed-off-by: Liam <liam@mail.com>

--- proxy.php
+++ proxy.php
@@ -1,164 +1,109 @@
 <?php
-/**
- * AJAX Cross Domain (PHP) Proxy 0.8
- * Copyright (C) 2016 Iacovos Constantinou (https://github.com/softius)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-/**
- * Enables or disables filtering for cross domain requests.
- * Recommended value: true
- */
-define('CSAJAX_FILTERS', true);
-/**
- * If set to true, $valid_requests should hold only domains i.e. a.example.com, b.example.com, usethisdomain.com
- * If set to false, $valid_requests should hold the whole URL ( without the parameters ) i.e. http://example.com/this/is/long/url/
- * Recommended value: false (for security reasons - do not forget that anyone can access your proxy)
- */
-define('CSAJAX_FILTER_DOMAIN', false);
-/**
- * Set debugging to true to receive additional messages - really helpful on development
- */
-define('CSAJAX_DEBUG', false);
-/**
- * A set of valid cross domain requests
- */
-$valid_requests = array(
-    // 'example.com'
-);
-/**
- * Set extra multiple options for cURL
- * Could be used to define CURLOPT_SSL_VERIFYPEER & CURLOPT_SSL_VERIFYHOST for HTTPS
- * Also to overwrite any other options without changing the code
- * See http://php.net/manual/en/function.curl-setopt-array.php
- */
-$curl_options = array(
-    // CURLOPT_SSL_VERIFYPEER => false,
-    // CURLOPT_SSL_VERIFYHOST => 2,
-);
-/* * * STOP EDITING HERE UNLESS YOU KNOW WHAT YOU ARE DOING * * */
-// identify request headers
-$request_headers = array( );
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
-        $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
-        $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
-        if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
-            $request_headers[] = "$headername: $value";
-        }
-    }
-}
-// identify request method, url and params
-$request_method = $_SERVER['REQUEST_METHOD'];
-if ('GET' == $request_method) {
-    $request_params = $_GET;
-} elseif ('POST' == $request_method) {
-    $request_params = $_POST;
-    if (empty($request_params)) {
-        $data = file_get_contents('php://input');
-        if (!empty($data)) {
-            $request_params = $data;
-        }
-    }
-} elseif ('PUT' == $request_method || 'DELETE' == $request_method) {
-    $request_params = file_get_contents('php://input');
-} else {
-    $request_params = null;
-}
-// Get URL from `csurl` in GET or POST data, before falling back to X-Proxy-URL header.
-if (isset($_REQUEST['csurl'])) {
-    $request_url = urldecode($_REQUEST['csurl']);
-} elseif (isset($_SERVER['HTTP_X_PROXY_URL'])) {
-    $request_url = urldecode($_SERVER['HTTP_X_PROXY_URL']);
-} else {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-    header('Status: 404 Not Found');
-    $_SERVER['REDIRECT_STATUS'] = 404;
-    exit;
-}
-$p_request_url = parse_url($request_url);
-// csurl may exist in GET request methods
-if (is_array($request_params) && array_key_exists('csurl', $request_params)) {
-    unset($request_params['csurl']);
-}
-// ignore requests for proxy :)
-if (preg_match('!' . $_SERVER['SCRIPT_NAME'] . '!', $request_url) || empty($request_url) || count($p_request_url) == 1) {
-    csajax_debug_message('Invalid request - make sure that csurl variable is not empty');
-    exit;
-}
-// check against valid requests
-if (CSAJAX_FILTERS) {
-    $parsed = $p_request_url;
-    if (CSAJAX_FILTER_DOMAIN) {
-        if (!in_array($parsed['host'], $valid_requests)) {
-            csajax_debug_message('Invalid domain - ' . $parsed['host'] . ' does not included in valid requests');
-            exit;
-        }
-    } else {
-        $check_url = isset($parsed['scheme']) ? $parsed['scheme'] . '://' : '';
-        $check_url .= isset($parsed['user']) ? $parsed['user'] . ($parsed['pass'] ? ':' . $parsed['pass'] : '') . '@' : '';
-        $check_url .= isset($parsed['host']) ? $parsed['host'] : '';
-        $check_url .= isset($parsed['port']) ? ':' . $parsed['port'] : '';
-        $check_url .= isset($parsed['path']) ? $parsed['path'] : '';
-        if (!in_array($check_url, $valid_requests)) {
-            csajax_debug_message('Invalid domain - ' . $request_url . ' does not included in valid requests');
-            exit;
-        }
-    }
-}
-// append query string for GET requests
-if ($request_method == 'GET' && count($request_params) > 0 && (!array_key_exists('query', $p_request_url) || empty($p_request_url['query']))) {
-    $request_url .= '?' . http_build_query($request_params);
-}
-// let the request begin
-$ch = curl_init($request_url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);   // (re-)send headers
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     // return response
-curl_setopt($ch, CURLOPT_HEADER, true);       // enabled response headers
-// add data for POST, PUT or DELETE requests
-if ('POST' == $request_method) {
-    $post_data = is_array($request_params) ? http_build_query($request_params) : $request_params;
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,  $post_data);
-} elseif ('PUT' == $request_method || 'DELETE' == $request_method) {
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $request_method);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $request_params);
-}
-// Set multiple options for curl according to configuration
-if (is_array($curl_options) && 0 <= count($curl_options)) {
-    curl_setopt_array($ch, $curl_options);
-}
-// retrieve response (headers and content)
-$response = curl_exec($ch);
-curl_close($ch);
-// split response to header and content
-list($response_headers, $response_content) = preg_split('/(\r\n){2}/', $response, 2);
-// (re-)send the headers
-$response_headers = preg_split('/(\r\n){1}/', $response_headers);
-foreach ($response_headers as $key => $response_header) {
-    // Rewrite the `Location` header, so clients will also use the proxy for redirects.
-    if (preg_match('/^Location:/', $response_header)) {
-        list($header, $value) = preg_split('/: /', $response_header, 2);
-        $response_header = 'Location: ' . $_SERVER['REQUEST_URI'] . '?csurl=' . $value;
-    }
-    if (!preg_match('/^(Transfer-Encoding):/', $response_header)) {
-        header($response_header, false);
-    }
-}
-// finally, output the content
-print($response_content);
-function csajax_debug_message($message)
-{
-    if (true == CSAJAX_DEBUG) {
-        print $message . PHP_EOL;
-    }
-}
+	/*
+	    The MIT License (MIT)
+	    Copyright (c) 2014 Oliver Moran
+	    Permission is hereby granted, free of charge, to any person obtaining a copy of
+	    this software and associated documentation files (the "Software"), to deal in
+	    the Software without restriction, including without limitation the rights to
+	    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+	    of the Software, and to permit persons to whom the Software is furnished to do
+	    so, subject to the following conditions:
+	    The above copyright notice and this permission notice shall be included in all
+	    copies or substantial portions of the Software.
+	    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	    SOFTWARE.
+	*/
+	// Used to enable cross-domain AJAX calls.
+	// Example: index.php?url=http://www.example.org/resource.json
+	$url = $_REQUEST["url"];
+	
+	if (substr ($url, 0, 7) != "http://"
+		&& substr ($url, 0, 8) != "https://"
+		&& substr ($url, 0, 6) != "ftp://") {
+		// NB: only absolute URLs are allowed -
+		// otherwise the script could be used to access local-to-file system files
+		die("ERROR: The argument 'url' must be an absolute URL beginning with 'http://', 'https://', or 'ftp://'.");
+	}
+	// temporarily override CURLs user agent with the user's own
+	ini_set("user_agent", $_SERVER['HTTP_USER_AGENT']);
+	// enable access from all domains
+	enable_cors();
+	switch ($_SERVER["REQUEST_METHOD"]) {
+		case "GET":
+			get($url);
+			break;
+		default:
+			post($url);
+			break;
+	}
+	// get the contents of the URL and echo the results
+	function get($url) {
+		// if (substr ($url, 0, 8) == "https://") {
+		//	echo getSSL($url);
+		// } else {
+			echo file_get_contents($url);
+		// }
+	}
+	// gets over HTTPS
+	function getSSL($url) {
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_HEADER, false);
+	    curl_setopt($ch, CURLOPT_URL, $url);
+	    curl_setopt($ch, CURLOPT_SSLVERSION,3); 
+	    $result = curl_exec($ch);
+	    curl_close($ch);
+	    return $result[0];
+	}
+	// post (or put or delete?) the encoded form to the URL and echo the results
+	function post($url) {
+		$postdata = http_build_query(
+		    array()
+		);
+		$opts = array('http' =>
+		    array(
+		        'method'  => $_SERVER['REQUEST_METHOD'],
+		        'header'  => 'Content-type: application/x-www-form-urlencoded',
+		        'content' => $postdata
+		    )
+		);
+		$context  = stream_context_create($opts);
+		// get the contents of the external URL and echo it
+		echo file_get_contents($url, false, $context);
+	}
+	/**
+	 *  An example CORS-compliant method.  It will allow any GET, POST, or OPTIONS requests from any
+	 *  origin.
+	 *
+	 *  In a production environment, you probably want to be more restrictive, but this gives you
+	 *  the general idea of what is involved.  For the nitty-gritty low-down, read:
+	 *
+	 *  - https://developer.mozilla.org/en/HTTP_access_control
+	 *  - http://www.w3.org/TR/cors/
+	 *
+	 */
+	function enable_cors() {
+		// Allow from any origin
+		if (isset($_SERVER['HTTP_ORIGIN'])) {
+			header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+			header('Access-Control-Allow-Credentials: true');;
+			header('Access-Control-Max-Age: 86400');	// cache for 1 day
+		} else {
+			header("Access-Control-Allow-Origin: *");
+			header('Access-Control-Allow-Credentials: true');;
+			header('Access-Control-Max-Age: 86400');	// cache for 1 day
+		}
+		// Access-Control headers are received during OPTIONS requests
+		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+				header("Access-Control-Allow-Methods: GET, POST, OPTIONS");		 
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+				header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+			exit(0);
+		}
+	}
+?>
