274fcdca1521d0dd9c6c17075f4441919acbcb33
byrnli <liam.byrne@abs.gov.au>
2018-07-24 16:42:45 +1000

    Update forex.php

--- forex.php
+++ forex.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
 #load{
-font-size: 1em;
+font-size: 0.8em;
 }
 #demo{
 	border-radius: 25px;
@@ -21,21 +21,39 @@ font-size: 1em;
 <body>
 <div id="demo">
 <span id="load">
-1 USD is 
+1 AUD is 
 <?php
 $url = 'https://openexchangerates.org/api/latest.json?app_id=013a14a8528e43259be556f393e6d5a3'; // path to your JSON file
 $data = file_get_contents($url); // put the contents of the file into a variable
 $forexdata = json_decode($data); // decode the JSON feed
 $rates = $forexdata->rates;
-echo $rates->MXN; ?>
+$audmxn = round( ($rates->MXN / $rates->AUD), 2);
+echo $audmxn; ?>
  Mexican Pesos
 </span>
+<br>
 <span id="display">
 1 AUD is
 <?php
-$audpen = $rates->PEN / $rates->AUD;
+$audpen = round( ($rates->PEN / $rates->AUD), 2);
 echo $audpen; ?>
 Peruvian Sol
+</span>
+<br>
+<span id="display">
+1 AUD is
+<?php
+$audcop = round( ($rates->COP / $rates->AUD), 2);
+echo $audcop; ?>
+Colombian Sol
+</span>
+<br>
+<span id="display">
+1 AUD is
+<?php
+$audusd = round( ($rates->USD / $rates->AUD), 2);
+echo $audusd; ?>
+USD
 </span>
 </div>
 </body>
